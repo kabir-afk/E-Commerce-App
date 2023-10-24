1adabbdc4011d3d511e11d7262639f14e017fc23
kabir-afk <kabirsingh12b@gmail.com>
2023-10-24 21:06:56 +0530

    made navbar look more like design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,18 +3,26 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <>
+    <header>
       <nav className="navbar">
-        <li><img src="/images/logo.svg" alt="sneakers-company-logo" /></li>
+        <li id="logo">
+          <img src="/images/logo.svg" alt="sneakers-company-logo" />
+        </li>
         <li>Collections</li>
         <li>Men</li>
         <li>Women</li>
         <li>About</li>
         <li>Contact</li>
-        <li className="ml"><img src="/images/icon-cart.svg" alt="cart-icon" /></li>
-        <li><div className="avatar"><img src="/images/image-avatar.png" alt="avatar-image" /></div></li>
+        <li className="ml">
+          <img src="/images/icon-cart.svg" alt="cart-icon" className="cart"/>
+        </li>
+        <li id="avatar">
+          <div className="avatar">
+            <img src="/images/image-avatar.png" alt="avatar-image" />
+          </div>
+        </li>
       </nav>
-    </>
+    </header>
   );
 }
 
