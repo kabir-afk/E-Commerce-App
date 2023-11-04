2e1ac85dacb922116db5c345e279731a4c5336a9
kabir-afk <kabirsingh12b@gmail.com>
2023-11-04 13:09:52 +0530

    tried adding the delete functionality but failed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,44 +2,53 @@ import React, { useState } from "react";
 import "./Navbar.css";
 
 function Navbar(props) {
+  const[isDelete,setIsDelete] = useState(false);
   const [isCheckoutVisible, setCheckoutVisible] = useState("hide");
   let [isCartEmpty, setCartEmpty] = useState(<p>Your cart is empty</p>);
+  const deleteItems = () => {
+    // setCartEmpty(<p className="empty-cart">Your cart is empty</p>);
+    setIsDelete(true);
+    console.log({isDelete});
+  };
   const toggleCheckout = () => {
     isCheckoutVisible === "hide"
       ? setCheckoutVisible("unhide")
       : setCheckoutVisible("hide");
 
-    props.cart !== 0
-      ? setCartEmpty(
-          <>
-            <div className="checkout-flex">
-              <div className="checkout-thumbnail-img">
-                <img
-                  src="images/image-product-1-thumbnail.jpg"
-                  alt="prod-1-thumbnail"
-                />
-              </div>
-              <div>
-                <p>Fall Limited Edtion Sneakers</p>
-                <p>
-                  $125.00 x {props.cart}{" "}
-                  <span>${(props.cart * 125.0).toFixed(2)}</span>
-                </p>
-              </div>
-              <div className="dustbin">
-                <img src="images/icon-delete.svg" alt="dustbin" />
-              </div>
+    if (props.cart !== 0) {
+      setCartEmpty(
+        <>
+          <div className="checkout-flex">
+            <div className="checkout-thumbnail-img">
+              <img
+                src="images/image-product-1-thumbnail.jpg"
+                alt="prod-1-thumbnail"
+              />
             </div>
-            <button>Checkout</button>
-          </>
-        )
-      : setCartEmpty(<p className="empty-cart">Your cart is empty</p>);
+            <div>
+              <p>Fall Limited Edtion Sneakers</p>
+              <p>
+                $125.00 x {props.cart}{" "}
+                <span>${(props.cart * 125.0).toFixed(2)}</span>
+              </p>
+            </div>
+            <button className="dustbin" onClick={deleteItems}>
+              <img src="images/icon-delete.svg" alt="dustbin" />
+            </button>
+          </div>
+          <button>Checkout</button>
+        </>
+      );
+    } 
+    else if (props.cart === 0 || isDelete === true) {
+      setCartEmpty(<p className="empty-cart">Your cart is empty</p>);
+    }
   };
   return (
     <header>
       <nav className="navbar">
         <li id="logo">
-          <img src="/images/logo.svg" alt="sneakers-company-logo" />
+          <div className="logo"><img src="/images/logo.svg" alt="sneakers-company-logo" /></div>
         </li>
         <li>Collections</li>
         <li>Men</li>
@@ -47,12 +56,13 @@ function Navbar(props) {
         <li>About</li>
         <li>Contact</li>
         <li className="ml">
-          <img
-            src="/images/icon-cart.svg"
-            alt="cart-icon"
-            className="cart"
-            onClick={toggleCheckout}
-          />
+          <div className="cart">
+            <img
+              src="/images/icon-cart.svg"
+              alt="cart-icon"
+              onClick={toggleCheckout}
+            />
+          </div>
           <span className="cart-items">{props.cart}</span>
         </li>
         <li id="avatar">
