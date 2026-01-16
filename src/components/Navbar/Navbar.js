0740a163964b1509fe33cc52722bcf2f21499662
Kabir Singh <kabirsingh12b@gmail.com>
2026-01-16 21:39:28 +0530

    fix : react logic

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,55 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 
-function Navbar(props) {
-  const [isDelete, setIsDelete] = useState(false);
-  const [isCheckoutVisible, setCheckoutVisible] = useState("hide");
-  let [isCartEmpty, setCartEmpty] = useState(<p>Your cart is empty</p>);
-  let [hamburgerIcon, setIconTo] = useState("menu");
-  let [alt, setAlt] = useState("hamburger-icon");
-  let[sideBarWidth,setSideBarWidth] = useState('hide-sidebar');
-  const deleteItems = () => {
-    // setCartEmpty(<p className="empty-cart">Your cart is empty</p>);
-    setIsDelete(true);
-    console.log({ isDelete });
-  };
-  const toggleCheckout = () => {
-    isCheckoutVisible === "hide"
-      ? setCheckoutVisible("unhide")
-      : setCheckoutVisible("hide");
-
-    if (props.cart !== 0) {
-      setCartEmpty(
-        <>
-          <div className="checkout-flex">
-            <div className="checkout-thumbnail-img">
-              <img
-                src="images/image-product-1-thumbnail.jpg"
-                alt="prod-1-thumbnail"
-              />
-            </div>
-            <div>
-              <p>Fall Limited Edition Sneakers</p>
-              <p>
-                $125.00 x {props.cart}{" "}
-                <span>${(props.cart * 125.0).toFixed(2)}</span>
-              </p>
-            </div>
-            <button className="dustbin" onClick={deleteItems}>
-              <img src="images/icon-delete.svg" alt="dustbin" />
-            </button>
-          </div>
-          <button>Checkout</button>
-        </>
-      );
-    } else if (props.cart === 0 || isDelete === true) {
-      setCartEmpty(<p className="empty-cart">Your cart is empty</p>);
-    }
-  };
+function Navbar({ cart, handleDelete }) {
+  const [isCheckoutVisible, setCheckoutVisible] = useState(false);
+  const [cartItems, setCartItems] = useState(0);
+  const [hamburgerIcon, setIconTo] = useState("menu");
+  const [alt, setAlt] = useState("hamburger-icon");
+  const [sideBarWidth, setSideBarWidth] = useState("hide-sidebar");
+  useEffect(() => {
+    setCartItems(cart);
+  }, [cart]);
   const changeIcon = () => {
-    hamburgerIcon === "menu" ? setIconTo("close") : setIconTo("menu");
-    alt === "hamburger-icon" ? setAlt("cross-icon") : setAlt("hamburger-icon");
-    sideBarWidth === 'hide-sidebar' ? setSideBarWidth('unhide-sidebar') : setSideBarWidth('hide-sidebar');
+    setIconTo(hamburgerIcon === "menu" ? "close" : "menu");
+    setAlt(alt === "hamburger-icon" ? "cross-icon" : "hamburger-icon");
+    setSideBarWidth(
+      sideBarWidth === "hide-sidebar" ? "unhide-sidebar" : "hide-sidebar"
+    );
   };
   return (
     <header>
@@ -57,12 +23,12 @@ function Navbar(props) {
         <ul className="navbar">
           <li>
             <button id="hamburger-icon" onClick={changeIcon}>
-              <img src={`images/icon-${hamburgerIcon}.svg`} alt={alt}/>
+              <img src={`/images/icon-${hamburgerIcon}.svg`} alt={alt} />
             </button>
           </li>
           <li id="logo">
             <div className="logo">
-              <img src="images/logo.svg" alt="sneakers-company-logo" />
+              <img src="/images/logo.svg" alt="sneakers-company-logo" />
             </div>
           </li>
           <li>Collections</li>
@@ -73,25 +39,51 @@ function Navbar(props) {
           <li className="ml">
             <div className="cart">
               <img
-                src="images/icon-cart.svg"
+                src="/images/icon-cart.svg"
                 alt="cart-icon"
-                onClick={toggleCheckout}
+                onClick={() => setCheckoutVisible(!isCheckoutVisible)}
               />
             </div>
-            <span className="cart-items">{props.cart}</span>
+            <span className="cart-items">{cart}</span>
           </li>
           <li id="avatar">
             <div className="avatar">
-              <img src="images/image-avatar.png" alt="avatar" />
+              <img src="/images/image-avatar.png" alt="avatar" />
             </div>
           </li>
         </ul>
       </nav>
-      <div className={`checkout ${isCheckoutVisible}`}>
-        <span>Cart</span>
-        <hr />
-        {isCartEmpty}
-      </div>
+      {isCheckoutVisible && (
+        <div className={"checkout"}>
+          <span>Cart</span>
+          <hr />
+          {cartItems !== 0 ? (
+            <div>
+              <div className="checkout-flex">
+                <div className="checkout-thumbnail-img">
+                  <img
+                    src="/images/image-product-1-thumbnail.jpg"
+                    alt="prod-1-thumbnail"
+                  />
+                </div>
+                <div>
+                  <p>Fall Limited Edition Sneakers</p>
+                  <p>
+                    $125.00 x {cartItems}{" "}
+                    <span>${(cartItems * 125.0).toFixed(2)}</span>
+                  </p>
+                </div>
+                <button className="dustbin" onClick={handleDelete}>
+                  <img src="/images/icon-delete.svg" alt="dustbin" />
+                </button>
+              </div>
+              <button>Checkout</button>
+            </div>
+          ) : (
+            <p className="empty-cart">Your cart is empty</p>
+          )}
+        </div>
+      )}
       <div className={`sidebar ${sideBarWidth}`}>
         <div className="sidebar-white-bg">
           <p>Collections</p>
